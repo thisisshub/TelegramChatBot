f3cf12b093ac374c8b81fffc6d2747a4072ff6e0
thisisshub <thisisshub@gmail.com>
2021-11-30 15:46:02 +0530

    Create echobot.py

---
+++ echobot.py
@@ -0,0 +1,40 @@
+import json, requests
+
+TOKEN = ""
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+
+def get_url(url):
+    """Downloads the content from a URL and returns a string"""
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+def get_json_from_url(url):
+    """Gets the string response as above and parses this into a python dictionary"""
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_updates():
+    """Retrieves a list of messages sent to our bot"""
+    url = URL + "getUpdates"
+    js = get_json_from_url(url)
+    return js
+
+
+def get_last_chat_id_and_text(updates):
+    """Get the chat ID and the message text of the most recent message sent to our bot"""
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return (text, chat_id)
+
+
+def send_message(text, chat_id):
+    """Sends the message to a particular chat id"""
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
